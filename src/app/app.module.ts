477110e9c4b95cf47780ec89e3278bf669234afd
arief.prakoso <arief.prakoso@btpn.com>
2020-07-08 21:38:30 +0700

    Prettier to have 2 line as tabwidth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-    declarations: [AppComponent, ServerComponent, ServersComponent],
-    imports: [BrowserModule],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/servers/servers.component.spec.ts
+++ src/app/servers/servers.component.spec.ts
@@ -8,9 +8,8 @@ describe('ServersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServersComponent ]
-    })
-    .compileComponents();
+      declarations: [ServersComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
