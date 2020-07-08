8f2ad6f55b2c398be0fb2ce20d3cd60ae4392b69
arief.prakoso <arief.prakoso@btpn.com>
2020-07-09 06:30:28 +0700

    Different implementation of selector and template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
+  // selector: '[app-servers]', -> can make app-servers becomes attribute of an element
+  // tslint:disable-next-line:component-selector
+  // selector: '.app-servers', -> can make app-servers becomes a class of an element
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+  <app-server></app-server>`,
+  // templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
