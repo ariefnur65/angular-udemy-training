c651f53bdd2fe4d87a23beba68f467ccac580af1
arief.prakoso <arief.prakoso@btpn.com>
2020-07-11 18:28:34 +0700

    Refactore line testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,25 +2,21 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('App Component', () => {
+  let fixture: any;
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
     });
+    fixture = TestBed.createComponent(AppComponent);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render app servers', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('app-servers')).toBeTruthy();
   });
 });
-
-
-
-
